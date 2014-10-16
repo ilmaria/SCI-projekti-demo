f15a747152a622a7ce039720da86b90d6b213441
HyApp <empires.hyapp@gmail.com>
2014-10-16 16:19:05 +0300

    hahmottelua

---
+++ Error/Error/WareHouse.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Error
+{
+    public class WareHouse
+    {
+        // public hyllysijainti(tuotekoodi)
+        //  koodi/hyllysijanti/fyysinen sijainti --> koodi/hylly/fyysinen sij/inventaariostatus
+    }
+}
